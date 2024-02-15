15a92e1549a38f48fdba2569e9f778aec5cea68c
Thomas Weholt <2002@weholt.org>
2024-02-15 16:21:04 +0100

    Inital commit: created default site.

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-o@%fc)@e--z+rg@g0z4u@a389_n8y!v(%9+ua=setu)0szc^yn"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
